8d1fb383c0098cac7fe4915373407b8d444b20a6
unknown <saurabh.find@gmail.com>
2018-08-11 18:48:52 +0530

    navigation

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CustomMaterialModule } from '../custom-material';
 
 @NgModule({
   imports: [
-    CommonModule
+    FormsModule, CommonModule, RouterModule, CustomMaterialModule
   ],
   declarations: [RegistrationComponent],
   exports: [RegistrationComponent]
