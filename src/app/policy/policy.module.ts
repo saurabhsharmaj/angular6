063cb88b1089293fb9523682ce9fe7362387a83a
unknown <saurabh.find@gmail.com>
2018-08-11 19:55:57 +0530

    navigation

--- src/app/policy/policy.module.ts
+++ src/app/policy/policy.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PolicyComponent } from './policy.component';
+import { CustomMaterialModule } from '../custom-material';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    FormsModule, CommonModule, RouterModule, CustomMaterialModule
   ],
   declarations: [PolicyComponent],
   exports: [PolicyComponent]
